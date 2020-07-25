0654a7f1a195bfe619a269b686a73171e2ca4c43
ari <ari@.com>
2020-07-25 20:35:24 +0300

    updating server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppComponent } from './app.component';
@@ -9,11 +11,13 @@ import { FirstFormComponent } from './first-form/first-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstFormComponent
+    FirstFormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first-form/first-form.component.ts
+++ src/app/first-form/first-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { EnrollmentService } from '../enrollment.service';
 
 @Component({
   selector: 'app-first-form',
@@ -8,10 +9,30 @@ import { User } from '../user';
 })
 export class FirstFormComponent implements OnInit {
   topics = ['angular','python','java'];
-  userModel = new User('','ttttt',22222,'','morning',true)
-  constructor() { }
+  userModel = new User('aron','vvvv',1234567890,'defult','morning',true);
+  isHasError = true;
+
+  constructor( private enro:EnrollmentService) { }
 
   ngOnInit(): void {
   }
+  checkValidation(value){
+    if(value==='defult'){
+      return this.isHasError =true
+    }else{
+      this.isHasError = false;
+    }
+
+  }
+  onSubmit(){
+    console.log(this.userModel);
+    this.enro.enroll(this.userModel)
+    .subscribe(
+      data => console.log('success',data),
+      error => console.log('success',error)
+    )
+    
+  }
+
 
 }
